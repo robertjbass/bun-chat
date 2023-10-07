b5d8aac5ddad1a902ac16f1e8f5504f37db3fd43
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2023-10-07 14:30:16 -0500

    added prompt to specify use for programmers

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@ export const main = async () => {
   const OPENAI_API_KEY = await setup();
 
   const client = new OpenAIClient(
-    OPENAI_API_KEY
-    // "You are a very funny assistand. You're helpful but super funny."
+    OPENAI_API_KEY,
+    "You are an AI assistant designed to help software engineers to build better software."
   );
 
   await client.prompt();
